5c05fc98b56f0c8fcd4929de661f25ae84e6e53d
Bianca Wilkinson <miztrz@gmail.com>
2020-07-21 03:48:58 +1000

    Added classes table

--- src/components/classes/Classes.js
+++ src/components/classes/Classes.js
@@ -5,7 +5,14 @@ const Classes = () => (
   <section id="class" className="wrapper style1 fullscreen fade-up">
     <div className="inner">
       <h1>Class Timetable</h1>
+      <div className="classes-container">
 
+        <span className="classes-item">Name</span>
+        <span className="classes-item">Day & Time</span>
+        <span className="classes-item">Level</span>
+        <span className="classes-item">Location</span>
+        <span className="classes-item">Cost</span>
+      </div>
     </div>
   </section>
 );
